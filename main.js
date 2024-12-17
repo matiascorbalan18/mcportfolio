eb1ab0c02119267231f55e1426b8f88bdabda7cf
Matias Corbalan <matt.corbalan@gmail.com>
2024-12-17 02:10:43 -0300

    twentyfourth commit

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ const hamMenu = document.querySelector(".ham-menu");
 
 const screenMenu = document.querySelector(".screen-menu");
 
+const menuLinks = document.querySelectorAll(".screen-menu a");
+
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     screenMenu.classList.toggle("active");
 })
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        hamMenu.classList.remove("active");
+        screenMenu.classList.remove("active");
+    });
+});
